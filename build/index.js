174dd168e77522426ec87a72a99a26afde9b0b38
Jure Taslak <jure.taslak@gmail.com>
2022-11-08 12:50:55 +0100

    Rework so we can use the program inside project

--- build/index.js
+++ build/index.js
@@ -1,96 +1,95 @@
-import { executionAsyncResource } from "async_hooks";
-import fs, { read } from "fs";
-import { exit } from "process";
-import readline from "readline";
-import { spawn } from 'child_process';
-import os from 'os';
-import yargs from 'yargs';
-import SimpleGit from 'simple-git';
-
-const argv = yargs
-  .option('name', {
-      description: 'The name',
-      alias: 'n',
-      type: 'string'
-  })
-  .option('route', {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const process_1 = require("process");
+const yargs_1 = __importDefault(require("yargs"));
+const argv = yargs_1.default
+    .option('name', {
+    description: 'The name',
+    alias: 'n',
+    type: 'string'
+})
+    .option('route', {
     alias: 'r',
     description: 'The API route to be generated',
     type: 'string'
-  })
-  .option('type', {
+})
+    .option('type', {
     alias: 't',
     description: 'Get or Post requst',
     type: 'string'
-  })
-  .option('path', {
+})
+    .option('path', {
     alias: 'p',
     description: 'The project path',
     type: 'string'
-  })
-  .help()
-  .alias('help', 'h')
-  .parseSync();
-
+})
+    .help()
+    .alias('help', 'h')
+    .parseSync();
 if (argv._[0] === "init") {
     console.log("Initializing project");
     initProject();
-} else if (argv._[0] === "generate-route") {
+}
+else if (argv._[0] === "generate-route") {
     if (!argv.path || !argv.name || !argv.route) {
         console.error("Need project path, route for API and name for controller");
-        exit(1);
+        (0, process_1.exit)(1);
     }
-    let type: "Get" | "Post" = "Get";
+    let type = "Get";
     if (argv.type && argv.type === "Get") {
         type = "Get";
-    } else if (argv.type && argv.type === "Post") {
+    }
+    else if (argv.type && argv.type === "Post") {
         type = "Post";
     }
     console.log("TODO Generating code for route: ", argv.route);
     generateApiRoute(argv.path, argv.name, argv.route, type);
 }
-
-function createFolderStructure(path: string, name: string) {
+function createFolderStructure(path, name) {
     try {
-        if (fs.existsSync(path + '/' + name)) {
+        if (fs_1.default.existsSync(path + '/' + name)) {
             console.error("Folder already exists!");
-            exit();
-        } else {
-            fs.mkdirSync(path + '/' + name);
-            fs.mkdirSync(path + '/' + name + '/src');
-            fs.mkdirSync(path + '/' + name + '/src/constants');
-            fs.mkdirSync(path + '/' + name + '/src/controllers');
-            fs.mkdirSync(path + '/' + name + '/src/engines');
-            fs.mkdirSync(path + '/' + name + '/src/services');
-            fs.mkdirSync(path + '/' + name + '/src/types');
-            fs.mkdirSync(path + '/' + name + '/build');
-            fs.mkdirSync(path + '/' + name + '/dist');
-            fs.mkdirSync(path + '/' + name + '/scripts');
-            fs.mkdirSync(path + '/' + name + '/static');
-            fs.mkdirSync(path + '/' + name + '/prisma');
+            (0, process_1.exit)();
         }
-    } catch (e) {
+        else {
+            fs_1.default.mkdirSync(path + '/' + name);
+            fs_1.default.mkdirSync(path + '/' + name + '/src');
+            fs_1.default.mkdirSync(path + '/' + name + '/src/constants');
+            fs_1.default.mkdirSync(path + '/' + name + '/src/controllers');
+            fs_1.default.mkdirSync(path + '/' + name + '/src/engines');
+            fs_1.default.mkdirSync(path + '/' + name + '/src/services');
+            fs_1.default.mkdirSync(path + '/' + name + '/src/types');
+            fs_1.default.mkdirSync(path + '/' + name + '/build');
+            fs_1.default.mkdirSync(path + '/' + name + '/dist');
+            fs_1.default.mkdirSync(path + '/' + name + '/scripts');
+            fs_1.default.mkdirSync(path + '/' + name + '/static');
+            fs_1.default.mkdirSync(path + '/' + name + '/prisma');
+        }
+    }
+    catch (e) {
         console.error(e);
     }
 }
-
 async function initProject() {
     const port = GetRandomInt(8000, 9900);
     try {
         const path = process.cwd();
-        if (!fs.existsSync(path + '/package.json')) {
+        if (!fs_1.default.existsSync(path + '/package.json')) {
             console.error("package.json file not found, please run `npm init` in the directory first to initialize npm project.");
-            exit(1);
+            (0, process_1.exit)(1);
         }
-        const packageJson = JSON.parse(fs.readFileSync(path + '/package.json', { encoding: 'utf8' }));
+        const packageJson = JSON.parse(fs_1.default.readFileSync(path + '/package.json', { encoding: 'utf8' }));
         const name = packageJson.name;
-
+        console.log(name);
         // createFolderStructure(path, name);
         // const git = SimpleGit(path + '/' + name);
         // await git.init();
         // const replaceName = new RegExp(/\$\{name\}/, 'g');
         // const replacePort = new RegExp(/\$\{port\}/, 'g');
-
         // const packageJsonTemplate = fs.readFileSync("./templates/package.json.template", { encoding: 'utf8' }).replaceAll(replaceName, name);
         // const tsconfigTemplate = fs.readFileSync("./templates/tsconfig.json.template", { encoding: 'utf8' }).replaceAll(replacePort, port.toString());
         // const tsoaTemplate = fs.readFileSync("./templates/tsoa.json.template", { encoding: 'utf8' }).replaceAll(replacePort, port.toString());
@@ -106,7 +105,6 @@ async function initProject() {
         // const prettierrcTemplate = fs.readFileSync("./templates/.prettierrc.json.template", { encoding: 'utf8' }).replaceAll(replacePort, port.toString()).replaceAll(replaceName, name);
         // const eslintignoreTemplate = fs.readFileSync("./templates/.eslintignore.template", { encoding: 'utf8' }).replaceAll(replacePort, port.toString()).replaceAll(replaceName, name);
         // const prettierignoreTemplate = fs.readFileSync("./templates/.prettierignore.template", { encoding: 'utf8' }).replaceAll(replacePort, port.toString()).replaceAll(replaceName, name);
-
         // fs.writeFileSync(path + '/' + name + '/package.json', packageJsonTemplate);
         // fs.writeFileSync(path + '/' + name + '/tsconfig.json', tsconfigTemplate);
         // fs.writeFileSync(path + '/' + name + '/tsoa.json', tsoaTemplate);
@@ -122,21 +120,17 @@ async function initProject() {
         // fs.writeFileSync(path + '/' + name + '/.prettierrc.json', prettierrcTemplate);
         // fs.writeFileSync(path + '/' + name + '/.eslintignore', eslintignoreTemplate);
         // fs.writeFileSync(path + '/' + name + '/.prettierignore.json', prettierignoreTemplate);
-
         // fs.openSync(path + '/' + name + '/prisma/dev.db', 'w');
-
         // Git stuff
         // await git.add(["package.json", "tsconfig.json", "tsoa.json", "src/", "scripts/", "prisma/", "static/", ".gitignore"]);
         // await git.commit(`Initializing project ${name}.\n\nUsing nodejs + express + tsoa + prisma`);
-
-    } catch (e) {
+    }
+    catch (e) {
         console.error(e);
     }
-
     console.log('Done!');
 }
-
-function generateApiRoute(projectPath: string, name: string, route: string, type: "Get" | "Post") {
+function generateApiRoute(projectPath, name, route, type) {
     try {
         const replaceName = RegExp(/\$\{name\}/, 'g');
         const replaceRoute = RegExp(/\$\{route\}/, 'g');
@@ -144,30 +138,25 @@ function generateApiRoute(projectPath: string, name: string, route: string, type
         const replaceEngineName = RegExp(/\$\{engineName\}/, 'g');
         const replaceEngineNameCapitalized = RegExp(/\$\{engineNameCapitalized\}/, 'g');
         const replaceType = RegExp(/\$\{type\}/, 'g');
-
         const controllerName = `${route}Controller`;
         const engineName = `${route}Engine`;
-
-        const controllerTemplate = fs.readFileSync("./templates/controller.ts.template", { encoding: 'utf8' })
+        const controllerTemplate = fs_1.default.readFileSync("./templates/controller.ts.template", { encoding: 'utf8' })
             .replaceAll(replaceName, name)
             .replaceAll(replaceRoute, route)
             .replaceAll(replaceControllerName, capitalize(controllerName))
             .replaceAll(replaceEngineName, engineName)
             .replaceAll(replaceEngineNameCapitalized, capitalize(engineName))
             .replaceAll(replaceType, type);
-
-        const engineTemplate = fs.readFileSync("./templates/engine.ts.template", { encoding: 'utf8' })
-            .replaceAll(replaceName, name)    
+        const engineTemplate = fs_1.default.readFileSync("./templates/engine.ts.template", { encoding: 'utf8' })
+            .replaceAll(replaceName, name)
             .replaceAll(replaceEngineName, capitalize(engineName));
-
-        fs.writeFileSync(projectPath + '/src/controllers/' + controllerName + '.ts', controllerTemplate);
-        fs.writeFileSync(projectPath + '/src/engines/' + engineName + '.ts', engineTemplate);
-
-    } catch (e) {
+        fs_1.default.writeFileSync(projectPath + '/src/controllers/' + controllerName + '.ts', controllerTemplate);
+        fs_1.default.writeFileSync(projectPath + '/src/engines/' + engineName + '.ts', engineTemplate);
+    }
+    catch (e) {
         console.error(e);
     }
 }
-
 /**
  * https://stackoverflow.com/a/1527820
  * Returns a random integer between min (inclusive) and max (inclusive).
@@ -176,12 +165,11 @@ function generateApiRoute(projectPath: string, name: string, route: string, type
  * lower than max if max isn't an integer).
  * Using Math.round() will give you a non-uniform distribution!
  */
- function GetRandomInt(min: number, max: number) {
+function GetRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-function capitalize(str: string) {
+function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+}
